46bbc9521c84e40aa8ef8977f2aa9587cb4fdd2f
Blase Moylan <moylanblase@gmail.com>
2023-06-12 17:27:48 -0700

    📦 NEW: setting up sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import './sidebar.css'
+import SidebarButton from './sidebarButton'
 export default function Sidebar(){
     return(
-        <div className='sidebar-container'>Sidebar</div>
+        <div className='sidebar-container'>
+            <img src='https://i.pinimg.com/originals/c8/eb/25/c8eb256d440f54a163c7662c21137fb4.jpg' 
+            className='profile-img' 
+            alt='profile'
+            />
+            <div>
+                <SidebarButton title="" to="" icon=""/>
+                <SidebarButton/>
+                <SidebarButton/>
+                <SidebarButton/>
+                <SidebarButton/>
+            </div>
+            <SidebarButton/>
+        </div>
     )
 }
