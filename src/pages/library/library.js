9706a89b003dafd2e4e677c6a9b76396d4ce26ef
Blase Moylan <moylanblase@gmail.com>
2023-06-19 17:55:15 -0700

    📦 NEW: adding more styling

--- src/pages/library/library.js
+++ src/pages/library/library.js
@@ -2,6 +2,9 @@ import React from 'react'
 import APIKit from '../../spotify'
 import { useState,useEffect} from 'react'
 import './library.css'
+import {CgPlayButtonR} from 'react-icons/cg'
+import { IconBase } from 'react-icons/lib'
+import { IconContext } from 'react-icons'
 
 export default function Library(){
     const [playlist, setPlaylist]=useState(null)
@@ -19,7 +22,13 @@ export default function Library(){
                 <div className='playlist-card'>
                     {/* <img src={playlist.images[0].url} className='playlist-image' alt="playlist img"/> */}
                     {playlist.images[0]?<img src={playlist.images[0].url} className='playlist-image' alt="playlist img"/>:<img src="https://th.bing.com/th/id/R.0c12d2fd4673dee91a743aecb340d6e6?rik=B1IyQZsF2%2fF6vA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_148071.png&ehk=wacpDthkV%2fQSfOLelWSsYQFabLKAVjXh1pZAq0%2fe3gU%3d&risl=&pid=ImgRaw&r=0" className='playlist-image-non' alt="playlist img"/>}
-                    {playlist.name}
+                        <p className='playlist-name'>{playlist.name}</p>
+                        <div className='playButton'>
+                            <IconContext.Provider value={{ size:"10%"}}>
+                                <CgPlayButtonR/>
+                            </IconContext.Provider>
+                        </div>
+                    
                     
                 </div>
             ))}
