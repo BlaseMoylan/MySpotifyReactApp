0628346272e3302a8a4d2755c9c3362c0a9aa966
Blase Moylan <moylanblase@gmail.com>
2023-06-20 11:31:38 -0700

    📦 NEW:player set up

---
+++ src/components/songCard/songCard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./SongCard.css"
+
+export default function SongCard(){
+    return <div>SongCard</div>
+}
+
+
--- src/pages/player/player.js
+++ src/pages/player/player.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./player.css"
+import { useLocation } from 'react-router-dom'
+import apiClient from '../../spotify'
 
 export default function Player(){
+    const location = useLocation()
+    const [tracks,setTracks]=useState([])
+    const [currentTrack,setCurrentTrack]=useState({})
+    const [currentIndex,setCurrentIndex]=useState(0)
+
+    useEffect(()=>{
+        if(location.state){
+            apiClient
+                .get("playlists/"+location.state?.id+"/tracks")
+                .then(res=>{
+                    setTracks(res.data.items);
+                    setCurrentTrack(res,data.items[0].track)
+                })
+        }
+    },[location.state])
     return(
-        <div className='page-container'>Player</div>
+        <div className='page-container'>
+            <div className='left-player-body'>
+
+            </div>
+            <div className='right-player-body'>
+                <SongCard />
+                <Queue />
+            </div>
+        </div>
     )
 }
