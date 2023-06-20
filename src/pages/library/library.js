7fa6395569601fee573c0f5fe1e0a4ccb72a1ff5
Blase Moylan <moylanblase@gmail.com>
2023-06-20 11:06:44 -0700

    📦 NEW: finish with library css

--- src/pages/library/library.js
+++ src/pages/library/library.js
@@ -5,6 +5,7 @@ import './library.css'
 import {CgPlayButtonR} from 'react-icons/cg'
 import { IconBase } from 'react-icons/lib'
 import { IconContext } from 'react-icons'
+import { useNavigate } from 'react-router-dom'
 
 export default function Library(){
     const [playlist, setPlaylist]=useState(null)
@@ -14,24 +15,28 @@ export default function Library(){
             console.log(responce.data.items)
     })
     },[])
+    const navigate=useNavigate()
+    const playPlaylist=(id)=>{
+        navigate("/player",{state:{id:id}})
+    }
     
     return(
         <div className='page-container'>
             <div className='library-body'>
-            {playlist?.map((playlist)=>(
-                <div className='playlist-card'>
-                    {/* <img src={playlist.images[0].url} className='playlist-image' alt="playlist img"/> */}
-                    {playlist.images[0]?<img src={playlist.images[0].url} className='playlist-image' alt="playlist img"/>:<img src="https://th.bing.com/th/id/R.0c12d2fd4673dee91a743aecb340d6e6?rik=B1IyQZsF2%2fF6vA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_148071.png&ehk=wacpDthkV%2fQSfOLelWSsYQFabLKAVjXh1pZAq0%2fe3gU%3d&risl=&pid=ImgRaw&r=0" className='playlist-image-non' alt="playlist img"/>}
-                        <p className='playlist-name'>{playlist.name}</p>
-                        <div className='playButton'>
-                            <IconContext.Provider value={{ size:"10%"}}>
-                                <CgPlayButtonR/>
-                            </IconContext.Provider>
-                        </div>
-                    
-                    
-                </div>
-            ))}
+                {playlist?.map((playlist)=>(
+                    <div className='playlist-card' key={playlist.id} onClick={()=> playPlaylist(playlist.id)}>
+                        {/* <img src={playlist.images[0].url} className='playlist-image' alt="playlist img"/> */}
+                        {playlist.images[0]?<img src={playlist.images[0].url} className='playlist-image' alt="playlist img"/>:<img src="https://th.bing.com/th/id/R.0c12d2fd4673dee91a743aecb340d6e6?rik=B1IyQZsF2%2fF6vA&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_148071.png&ehk=wacpDthkV%2fQSfOLelWSsYQFabLKAVjXh1pZAq0%2fe3gU%3d&risl=&pid=ImgRaw&r=0" className='playlist-image-non' alt="playlist img"/>}
+                            <p className='playlist-name'>{playlist.name}</p>
+                            <div className='playButton'>
+                                <IconContext.Provider value={{ size:"30%"}}>
+                                    <CgPlayButtonR/>
+                                </IconContext.Provider>
+                            </div>
+                        
+                        
+                    </div>
+                ))}
             </div>
             
         </div>
