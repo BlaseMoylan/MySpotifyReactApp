1b3a130679da15636cd306e6c582fe5aecc980ce
Blase Moylan <moylanblase@gmail.com>
2023-06-26 17:44:07 -0700

    📦 NEW: lots of css changes and fixing bugs related to play()

--- src/components/audioPlayer/controls.js
+++ src/components/audioPlayer/controls.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./controls.css"
+import { IconContext } from "react-icons";
+import {IoPlaySkipBack,IoPlaySkipForward,IoPlay} from "react-icons/io5"
+    import { formToJSON } from "axios";
 
-export default function Controls(){
+export default function Controls({isPlaying,setIsPlaying,handleNext,handlePrev}){
     return(
-        <div>Controls</div>
+        <IconContext.Provider value={{size:"35px",color:"#f0f8ff"}}>
+            <div className="cotrols-wrapper container">
+                <div className="action-btn" onClick={handlePrev}>
+                    <IoPlaySkipBack />
+                </div>
+                <div className="play-pause-btn container" onClick={()=>setIsPlaying(!isPlaying)}>
+                    <IoPlay />
+                </div>
+                <div className="action-btn container" onClick={handleNext}>
+                    <IoPlaySkipForward />
+                </div>
+
+            </div>
+        </IconContext.Provider>
     )
 }
