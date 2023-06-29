1e1404de869215edfc822310150adfa52bece4ef
Blase Moylan <moylanblase@gmail.com>
2023-06-28 20:11:48 -0700

    🐛 FIX: figuring out how to show the song that is currently playing in the queue

--- src/components/queue/queue.js
+++ src/components/queue/queue.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./queue.css"
-
+// need to pass in isPlaying inorder to show which song is currently playing in the queue
 export default function Queue({tracks, setCurrentIndex}){
     return <div className='queue-container container'>
         <div className='queue container'>
@@ -16,3 +16,27 @@ export default function Queue({tracks, setCurrentIndex}){
         </div>
     </div>
 }
+// import React from 'react';
+// import "./queue.css"
+
+// export default function Queue({tracks, currentTrackIndex, setCurrentIndex}) {
+//     return (
+//         <div className='queue-container container'>
+//             <div className='queue container'>
+//                 <p className='upNext'>upNext</p>
+//                 <div className='queue-list'>
+//                     {tracks?.map((track, index) => (
+//                         <div
+//                             className={`queue-item container ${currentTrackIndex === index ? 'active' : ''}`}
+//                             onClick={() => setCurrentIndex(index)}
+//                             key={index}
+//                         >
+//                             <p className='trackName'>{track?.track?.name}</p>
+//                             <p className='time'>1:30</p>
+//                         </div>
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
