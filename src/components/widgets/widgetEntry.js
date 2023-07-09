a5fcedc11f0da6bcadb0987bb02454804bdb07cc
Blase Moylan <moylanblase@gmail.com>
2023-07-08 20:23:47 -0700

    📦 NEW: widget entry funtionality

--- src/components/widgets/widgetEntry.js
+++ src/components/widgets/widgetEntry.js
@@ -3,6 +3,13 @@ import "./widgetEntry.css"
 
 export default function WidgetEntry({title, subtitle, image}){
     return(
-        <div>WidgetEntry</div>
+        <div className="entry-body container">
+            <img src={image} alt={title} className="entry-image"/>
+            <div className="entry-right-body">
+                <p className="entry-title">{title}</p>
+                <p className="entry-subtitle">{subtitle}</p>
+            </div>
+
+        </div>
     )
 }
