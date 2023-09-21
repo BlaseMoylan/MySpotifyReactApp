35f769a8d13e9938054c97227f9f1c065b5bd35c
Blase Moylan <moylanblase@gmail.com>
2023-09-21 14:25:02 -0700

    📖 DOC: making notes for future

--- src/pages/favorites/favorites.js
+++ src/pages/favorites/favorites.js
@@ -5,13 +5,7 @@ import { useEffect } from 'react';
 
 export default function Favorites(){
     const navigate = useNavigate();
-    // Idea*
-        // make it so that when this tab is selected it will send you straight to the player
-        
-    // there are over 200 tracks being sent down think about how you want to display interact and play
-    // maybe connect up to player
-    // maybe display in here all of the albums that your favorite songs come from
-    // maybe (if possible) have a link to them so you can play them just like all the other albums
+    // need to make the connection faster
     useEffect(()=>{
         navigate('/player', { state: { id: null } })
     },[])
