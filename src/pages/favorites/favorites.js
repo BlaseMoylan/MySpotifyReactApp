162bcbc5758bb97f198b2609ad7bcce468d57de2
Blase Moylan <moylanblase@gmail.com>
2023-09-21 14:23:45 -0700

    📦 NEW: got the favorites tab working

--- src/pages/favorites/favorites.js
+++ src/pages/favorites/favorites.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './favorites.css'
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function Favorites(){
     const navigate = useNavigate();
@@ -11,9 +12,10 @@ export default function Favorites(){
     // maybe connect up to player
     // maybe display in here all of the albums that your favorite songs come from
     // maybe (if possible) have a link to them so you can play them just like all the other albums
+    useEffect(()=>{
+        navigate('/player', { state: { id: null } })
+    },[])
     return(
-        <div className='page-container'>{
-            navigate('/player', { state: { id: null } })
-          }</div>
+        <div className='page-container'></div>
     )
 }
